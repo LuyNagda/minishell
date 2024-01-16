24c3b3501648b15c5028fd061f1a308f25a56f19
Luy Nagda <lunagda@z3r11p5.42lyon.fr>
2024-01-16 17:20:10 +0100

    Reducing redundant code, adding specific errors when command not found

--- sources/exec/exec_pipex.c
+++ sources/exec/exec_pipex.c
@@ -6,7 +6,7 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 12:22:40 by lunagda           #+#    #+#             */
-/*   Updated: 2024/01/16 16:43:41 by lunagda          ###   ########.fr       */
+/*   Updated: 2024/01/16 17:17:58 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,24 @@
 
 //int	g_status_code;
 
+static void	exec_command(t_minishell *shell, t_commands *command, t_pipex *pipex)
+{
+	if (command->position != 0)
+		close(pipex->o_pipe[0]);
+	close(pipex->c_pipe[0]);
+	close(pipex->c_pipe[1]);
+	if (command->is_builtin)
+		exit(ft_dispatch_builtin(shell, command));
+	if (execve(command->path, command->arguments, pipex->envp) == -1)
+	{
+		if (command->path == NULL)
+			ft_printf("command not found: %s\n", command->arguments[0]);
+		else
+			ft_printf("%s: %s\n", strerror(errno), command->arguments[0]);
+	}
+	exit(EXIT_FAILURE);
+}
+
 void	child_one(t_minishell *shell, t_commands *command, t_pipex *pipex)
 {
 	if (shell->command_amount != 1)
@@ -36,15 +54,7 @@ void	child_one(t_minishell *shell, t_commands *command, t_pipex *pipex)
 			error_msg("DUP2 failed");
 		close(pipex->outfile);
 	}
-	close(pipex->c_pipe[0]);
-	close(pipex->c_pipe[1]);
-	if (command->path == NULL)
-		exit(127);
-	if (command->is_builtin)
-		exit(ft_dispatch_builtin(shell, command));
-	if (execve(command->path, command->arguments, pipex->envp) == -1)
-		ft_printf("%s: %s", strerror(errno), command->arguments[0]);
-	exit(EXIT_FAILURE);
+	exec_command(shell, command, pipex);
 }
 
 void	child_middle(t_minishell *shell, t_commands *command, t_pipex *pipex)
@@ -53,58 +63,18 @@ void	child_middle(t_minishell *shell, t_commands *command, t_pipex *pipex)
 		error_msg("DUP2 failed");
 	if (dup2(pipex->c_pipe[1], STDOUT_FILENO) == -1)
 		error_msg("DUP2 failed");
-	close(pipex->o_pipe[0]);
-	close(pipex->c_pipe[0]);
-	close(pipex->c_pipe[1]);
-	if (command->path == NULL)
-		exit(127);
-	if (command->is_builtin)
-		exit(ft_dispatch_builtin(shell, command));
-	if (execve(command->path,command->arguments, pipex->envp) == -1)
-		ft_printf("%s: %s", strerror(errno), command->arguments[0]);
-	exit(EXIT_FAILURE);
+	exec_command(shell, command, pipex);
 }
 
 void	child_last(t_minishell *shell, t_commands *command, t_pipex *pipex)
 {
 	if (dup2(pipex->o_pipe[0], STDIN_FILENO) == -1)
 		error_msg("DUP2 failed");
-	//if (ft_str_contains(command->raw_command, ">", 0))
-	//{
-	//	if (dup2(pipex->outfile, STDOUT_FILENO) == -1)
-	//		error_msg("DUP2 failed");
-	//	close(pipex->outfile);
-	//}
-	close(pipex->o_pipe[0]);
-    close(pipex->c_pipe[0]);
-    close(pipex->c_pipe[1]);
-	if (command->path == NULL)
-		exit(127);
-	if (command->is_builtin)
-		exit(ft_dispatch_builtin(shell, command));
-	if (execve(command->path, command->arguments, pipex->envp) == -1)
-		ft_printf("%s: %s", strerror(errno), command->arguments[0]);
-	exit(EXIT_FAILURE);
+	exec_command(shell, command, pipex);
 }
-
-#include <stdio.h>
 
 void	exec_cmd_loop(t_minishell *shell, t_commands *command, t_pipex *pipex)
 {
-	//if (has_redirection(command, '>') == 2)
-	//{
-	//	command->arguments = ft_split(command->raw_command, '>');
-	//	command->arguments = trim_command_list(command->arguments);
-	//	pipex->outfile = open(command->arguments[1], O_WRONLY | O_APPEND | O_CREAT, 0777);
-	//	command->arguments = ft_split(command->arguments[0], ' ');
-	//}
-	//else if (has_redirection(command, '>') == 1)
-	//{
-	//	command->arguments = ft_split(command->raw_command, '>');
-	//	command->arguments = trim_command_list(command->arguments);
-	//	pipex->outfile = open(command->arguments[1], O_CREAT | O_RDWR | O_TRUNC, 0777);
-	//	command->arguments = ft_split(command->arguments[0], ' ');
-	//}
 	if (pipe(pipex->c_pipe) == -1)
 		error_msg("Pipe");
 	pipex->pid[command->position] = fork();
@@ -117,12 +87,10 @@ void	exec_cmd_loop(t_minishell *shell, t_commands *command, t_pipex *pipex)
 	else if (command->position && (command->position == shell->command_amount - 1) && pipex->pid[command->position] == 0)
 		child_last(shell, command, pipex);
 	close(pipex->c_pipe[1]);
-	if (pipex->o_pipe[0] != -1)
+	if (pipex->o_pipe[0] != -1 || (command->position == shell->command_amount - 1))
 		close(pipex->o_pipe[0]);
 	if (!(command->position == shell->command_amount - 1))
 		pipex->o_pipe[0] = pipex->c_pipe[0];
-	else
-		close(pipex->c_pipe[0]);
 }
 
 void	exec_cmd(t_minishell *shell, t_commands *commands)
