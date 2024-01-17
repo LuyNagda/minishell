e051be40a235eb677d8d68165ca56a104f91c736
Luy Nagda <lunagda@z4r10p7.42lyon.fr>
2024-01-17 16:02:03 +0100

    Printing exit when exiting

--- sources/builtins/exit_builtin.c
+++ sources/builtins/exit_builtin.c
@@ -6,13 +6,15 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:25:39 by jbadaire          #+#    #+#             */
-/*   Updated: 2024/01/16 15:12:33 by lunagda          ###   ########.fr       */
+/*   Updated: 2024/01/17 16:01:19 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "ft_printf.h"
 
 void	exec_exit(t_minishell *shell)
 {
+	ft_printf("exit\n");
 	shell->is_running = _false;
 }
