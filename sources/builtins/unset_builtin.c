6652fde37bef6864ff4ae64845b97c54d26587b1
Luy Nagda <luy.linesh.nagda@gmail.com>
2024-01-18 20:18:52 +0100

    Fixing unset builtin

--- sources/builtins/unset_builtin.c
+++ sources/builtins/unset_builtin.c
@@ -6,7 +6,7 @@
 /*   By: luynagda <luynagda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:25:06 by jbadaire          #+#    #+#             */
-/*   Updated: 2024/01/17 21:52:36 by luynagda         ###   ########.fr       */
+/*   Updated: 2024/01/18 20:17:19 by luynagda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	exec_unset(t_minishell *shell, t_commands *command)
 {
-	shell->env_map = env_map_remove_from_key(shell->env_map, command->arguments[0]);
+	shell->env_map = env_map_remove_from_key(shell->env_map, command->arguments[1]);
 	if (shell->env_map == NULL)
 		env_map_replace_or_add(shell->env_map, "?", "1");
 	else
