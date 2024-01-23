80634cc02f462742c7c03ede4e1c104fad31eeb4
Luy Nagda <lunagda@z4r5p1.42lyon.fr>
2024-01-23 15:54:55 +0100

    Fixing echo builtin

--- sources/builtins/echo_builtin.c
+++ sources/builtins/echo_builtin.c
@@ -6,7 +6,7 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:23:40 by jbadaire          #+#    #+#             */
-/*   Updated: 2024/01/23 13:46:48 by lunagda          ###   ########.fr       */
+/*   Updated: 2024/01/23 15:54:12 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,12 @@ static int	check_arg(t_commands *command)
 	i = 1;
 	while (command->arguments[i])
 	{
-		j = 1;
-		while (command->arguments[i][0])
+		j = 0;
+		if (command->arguments[i][0] == '-')
+			j++;
+		while (command->arguments[i][j])
 		{
-			if (command->arguments[i][0] == '-')
-			{
-				if (!ft_strchr("n", command->arguments[i][j]))
-					return (i);
-			}
-			else
+			if (!ft_strchr("n", command->arguments[i][j]))
 				return (i);
 			j++;
 		}
