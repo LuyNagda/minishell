9113cd1a887d5ffaaa468203eeb07389a9139fdb
Luy Nagda <lunagda@z4r5p1.42lyon.fr>
2024-01-24 15:31:34 +0100

    Modularising the export builtin

--- sources/builtins/get_export_values.c
+++ sources/builtins/get_export_values.c
@@ -6,7 +6,7 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:25:50 by jbadaire          #+#    #+#             */
-/*   Updated: 2024/01/24 14:52:53 by lunagda          ###   ########.fr       */
+/*   Updated: 2024/01/24 15:30:57 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ static char **populate_export_without_value(t_commands *command, int *i, int *ha
 	export[1] = ft_strdup("");
 	export[2] = 0;
 	*has_equal = 1;
-	*i = *i + 2;
+	*i = *i + 1;
 	return (export);
 }
 
@@ -69,6 +69,7 @@ static char **populate_export(t_commands *command, int *i, int *has_equal)
 	else
 		*has_equal = 1;
 	*i = *i + 1;
+	return (export);
 }
 
 char	**get_export_values(t_commands *command, int *i, int *has_equal)
@@ -80,42 +81,11 @@ char	**get_export_values(t_commands *command, int *i, int *has_equal)
 	if (ft_str_contains(command->arguments[*i], "=",
 		ft_strlen(command->arguments[*i]) - 1)
 		&& ft_string_in_quotes(command->arguments[*i + 1]))
-	{
-		export = (char **)malloc(sizeof(char *) * 3);
-		if (!export)
-			return (NULL);
-		export[0] = ft_strtrim(command->arguments[*i], "=");
-		export[1] = ft_strtrim(command->arguments[*i + 1], "\"");
-		export[2] = 0;
-		*has_equal = 1;
-		*i = *i + 2;
-	}
+		return (populate_export_with_value(command, i, has_equal));
 	else if (ft_str_contains(command->arguments[*i], "=",
 		ft_strlen(command->arguments[*i]) - 1)
 		&& !ft_string_in_quotes(command->arguments[*i + 1]))
-	{
-		export = (char **)malloc(sizeof(char *) * 3);
-		export[0] = ft_strtrim(command->arguments[*i], "=");
-		export[1] = ft_strdup("");
-		export[2] = 0;
-		*has_equal = 1;
-		*i = *i + 1;
-	}
+		return (populate_export_without_value(command, i, has_equal));
 	else
-	{
-		export = ft_split(command->arguments[*i], '=');
-		if (!export[1])
-		{
-			ft_free_split(export);
-			export = (char **)malloc(sizeof(char *) * 3);
-			export[0] = ft_strdup(command->arguments[*i]);
-			export[1] = ft_strdup("");
-			export[2] = 0;
-			*has_equal = 0;
-		}
-		else
-			*has_equal = 1;
-		*i = *i + 1;
-	}
-	return (export);
+		return (populate_export(command, i, has_equal));
 }
