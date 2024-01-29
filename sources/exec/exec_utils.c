43728f53e73d2d3c5697ec415a6a51fd0ece3f05
Luy Nagda <lunagda@z3r4p6.42lyon.fr>
2024-01-29 18:13:54 +0100

    Removing readline and having get_next_line for here_doc

--- sources/exec/exec_utils.c
+++ sources/exec/exec_utils.c
@@ -6,19 +6,13 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 15:10:16 by jbadaire          #+#    #+#             */
-/*   Updated: 2024/01/26 15:30:43 by lunagda          ###   ########.fr       */
+/*   Updated: 2024/01/29 18:07:58 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "string_utils.h"
-#include "put_utils.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <errno.h>
 
 void	error_msg(char *string)
 {
@@ -35,53 +29,4 @@ void	free_and_exit(t_minishell *shell, t_pipex *pipex, int code)
 	free(pipex->pid);
 	free(shell->sended_line);
 	exit(code);
-}
-
-void	here_doc(t_minishell *shell, t_commands *command, t_pipex *pipex)
-{
-	char	*line;
-
-	if (has_heredoc(command, "<<") && command->arguments_amount != 1)
-	{
-		heredoc_parsing(shell, command, "<<", pipex);
-		line = readline("$>");
-		while (!ft_str_equals(command->here_doc, line))
-		{
-			ft_putstr_fd(line, command->input_fd);
-			ft_putstr_fd("\n", command->input_fd);
-			free(line);
-			line = readline("$>");
-		}
-		free(line);
-		close(command->input_fd);
-		command->input_fd = open(".here_doc", O_RDONLY);
-		if (command->input_fd < 0)
-		{
-			perror("here_doc");
-			free_and_exit(shell, pipex, 126);
-		}
-		if (dup2(command->input_fd, STDIN_FILENO) == -1)
-			error_msg("DUP2 failed");
-		close(command->input_fd);
-	}
-}
-
-void	normal_redirections(t_minishell *shell,
-		t_commands *command, t_pipex *pipex)
-{
-	if (has_redirection(command, '<'))
-	{
-		redirection_parsing(shell, command, "<", pipex);
-		if (command->input_fd > 0)
-		{
-			if (dup2(command->input_fd, STDIN_FILENO) == -1)
-				error_msg("DUP2 failed");
-			close(command->input_fd);
-		}
-		else
-		{
-			perror("bash");
-			free_and_exit(shell, pipex, errno);
-		}
-	}
 }
---
+++ sources/exec/heredoc_utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 18:05:56 by lunagda           #+#    #+#             */
+/*   Updated: 2024/01/29 18:13:16 by lunagda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "put_utils.h"
+#include "string_utils.h"
+#include "get_next_line.h"
+
+static void	here_doc_error_handling(t_minishell *shell, t_commands *command, t_pipex *pipex)
+{
+	if (command->input_fd < 0)
+	{
+		perror("here_doc");
+		free_and_exit(shell, pipex, 126);
+	}
+	if (dup2(command->input_fd, STDIN_FILENO) == -1)
+		error_msg("DUP2 failed");
+	close(command->input_fd);
+}
+
+void	here_doc(t_minishell *shell, t_commands *command, t_pipex *pipex)
+{
+	char	*line;
+
+	if (has_heredoc(command, "<<") && command->arguments_amount != 1)
+	{
+		heredoc_parsing(shell, command, "<<", pipex);
+		ft_putstr_fd("> ", 1);
+		line = get_next_line(0);
+		while (ft_strncmp(command->here_doc, line, ft_strlen(command->here_doc)))
+		{
+			ft_putstr_fd(line, command->input_fd);
+			free(line);
+			ft_putstr_fd("> ", 1);
+			line = get_next_line(0);
+		}
+		free(line);
+		close(command->input_fd);
+		command->input_fd = open(".here_doc", O_RDONLY);
+		here_doc_error_handling(shell, command, pipex);
+	}
+}
+
+void	normal_redirections(t_minishell *shell,
+		t_commands *command, t_pipex *pipex)
+{
+	if (has_redirection(command, '<'))
+	{
+		redirection_parsing(shell, command, "<", pipex);
+		if (command->input_fd > 0)
+		{
+			if (dup2(command->input_fd, STDIN_FILENO) == -1)
+				error_msg("DUP2 failed");
+			close(command->input_fd);
+		}
+		else
+		{
+			perror("bash");
+			free_and_exit(shell, pipex, errno);
+		}
+	}
+}
