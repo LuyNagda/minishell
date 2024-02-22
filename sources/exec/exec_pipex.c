d78adde1105b383578abbdbd44708f894715898d
Luy Nagda <luy2709@yahoo.com>
2024-02-22 18:03:46 +0000

    Fix: setting status_string outside the while loop for waiting on children

--- sources/exec/exec_pipex.c
+++ sources/exec/exec_pipex.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 12:22:40 by lunagda           #+#    #+#             */
-/*   Updated: 2024/02/22 17:44:13 by marvin           ###   ########.fr       */
+/*   Updated: 2024/02/22 18:02:17 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,22 +97,20 @@ void	exec_cmd_loop(t_minishell *shell, t_commands *command, t_pipex *pipex)
 static void	wait_for_children(t_minishell *shell, t_pipex *pipex)
 {
 	while (pipex->index < shell->command_amount)
+		waitpid(pipex->pid[pipex->index++], &pipex->status, 0);
+	if (WIFSIGNALED(pipex->status))
 	{
-		waitpid(pipex->pid[pipex->index++], &pipex->status, 0);	
-		if (WIFSIGNALED(pipex->status))
+		if (WTERMSIG(pipex->status) == 3)
 		{
-            if (WTERMSIG(pipex->status) == 3)
-			{
-				ft_putstr_fd("Quit (core dumped)", 2);
-				pipex->status_string = ft_strdup("131");
-			}
-			if (WTERMSIG(pipex->status) == 2)
-				pipex->status_string = ft_strdup("130");
-			ft_putstr_fd("\n", 2);
-        }
-		else
-			pipex->status_string = ft_itoa(WEXITSTATUS(pipex->status));
+			ft_putstr_fd("Quit (core dumped)", 2);
+			pipex->status_string = ft_strdup("131");
+		}
+		if (WTERMSIG(pipex->status) == 2)
+			pipex->status_string = ft_strdup("130");
+		ft_putstr_fd("\n", 2);
 	}
+	else
+		pipex->status_string = ft_itoa(WEXITSTATUS(pipex->status));
 }
 
 void	exec_cmd(t_minishell *shell, t_commands *commands)
@@ -124,7 +122,6 @@ void	exec_cmd(t_minishell *shell, t_commands *commands)
 	if (pipex.envp == NULL)
 		return ;
 	pipex.o_pipe[0] = -1;
-	pipex.status_string = NULL;
 	while (commands)
 	{
 		exec_cmd_loop(shell, commands, &pipex);
